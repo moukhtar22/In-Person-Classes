1b79f9865cc31f6382f1edac7cccb38ab25ab1bd
elithecomputerguy <60199254+elithecomputerguy@users.noreply.github.com>
2026-02-07 09:48:24 -0500

    new
    new

---
+++ Building AI Powered Telephone Agents with Twilio and ElevenLabs/twilio-outgoing.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+from twilio.rest import Client
+
+load_dotenv()
+
+account_sid = os.getenv("SID")
+auth_token = os.getenv("TOKEN")
+client = Client(account_sid, auth_token)
+
+# The phone number to call (recipient)
+to_phone_number = "+18283658190" # Replace with the actual recipient's number
+# Your Twilio phone number (must be purchased and voice-enabled in Twilio)
+from_phone_number = "+18444311372" # Replace with your Twilio number
+
+call = client.calls.create(
+    twiml='''<Response>
+                <Say>Hello, World. This is the Twilio bot calling!</Say>
+                <Pause length="2"/>
+                <Say>That was fun! Goodbye</Say>
+                <Pause length="2"/>
+            </Response>''',
+    to=to_phone_number,
+    from_=from_phone_number
+)
+
+print(call.sid)
